cf83822e9e1f3db68d2e97a6ec6c103ed35c4025
fark888 <159470733+fark888@users.noreply.github.com>
2024-08-11 16:28:45 +0000

    feat: initiated the project

--- scripts/mint.js
+++ scripts/mint.js
@@ -3,33 +3,33 @@ const fs = require("fs");
 const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
 
 const sendShieldedTransaction = async (signer, destination, data, value) => {
-  const rpcLink = hre.network.config.url;
-  const [encryptedData] = await encryptDataField(rpcLink, data);
-  return await signer.sendTransaction({
-    from: signer.address,
-    to: destination,
-    data: encryptedData,
-    value,
-  });
+      const rpcLink = hre.network.config.url;
+        const [encryptedData] = await encryptDataField(rpcLink, data);
+          return await signer.sendTransaction({
+                from: signer.address,
+                    to: destination,
+                        data: encryptedData,
+                            value,
+          });
 };
 
 async function main() {
-  const contractAddress = fs.readFileSync("contract.txt", "utf8").trim();
-  const [signer] = await hre.ethers.getSigners();
-  const contractFactory = await hre.ethers.getContractFactory("PERC20Sample");
-  const contract = contractFactory.attach(contractAddress);
-  const functionName = "mint100tokens";
-  const mint100TokensTx = await sendShieldedTransaction(
-    signer,
-    contractAddress,
-    contract.interface.encodeFunctionData(functionName),
-    0
-  );
-  await mint100TokensTx.wait();
-  console.log("Transaction Receipt: ", `Minting token has been success! Transaction hash: https://explorer-evm.testnet.swisstronik.com/tx/${mint100TokensTx.hash}`);
+      const contractAddress = fs.readFileSync("contract.txt", "utf8").trim();
+        const [signer] = await hre.ethers.getSigners();
+          const contractFactory = await hre.ethers.getContractFactory("PrivateNFT");
+            const contract = contractFactory.attach(contractAddress);
+              const functionName = "safeMint";
+                const safeMintTx = await sendShieldedTransaction(
+                        signer,
+                            contractAddress,
+                                contract.interface.encodeFunctionData(functionName, [signer.address, 1]),
+                                    0
+                );
+                  await safeMintTx.wait();
+                    console.log("Transaction Receipt: ", `Minting NFT has been success! Transaction hash: https://explorer-evm.testnet.swisstronik.com/tx/${safeMintTx.hash}`);
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+      console.error(error);
+        process.exitCode = 1;
 });
